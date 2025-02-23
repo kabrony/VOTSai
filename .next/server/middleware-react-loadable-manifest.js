26bec0bdb8a8686f784bfe3cf28154e6ebbc78b3
kabrony <aldo728@gmail.com>
2025-02-22 20:58:54 -0500

    Initial commit with flattened Streamlit app structure

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"app/page.tsx -> ../components/ChatWindow\":{\"id\":\"app/page.tsx -> ../components/ChatWindow\",\"files\":[\"static/chunks/_app-pages-browser_components_ChatWindow_tsx.js\"]}}"
